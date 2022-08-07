e860b2edcd80dd962fcea02ffd53d22f0910b12c
viacheslav Kurov <0990004793qw@gmail.com>
2022-08-07 13:51:24 +0300

    added fix style

--- script.js
+++ script.js
@@ -5,21 +5,18 @@ console.log(sumTo(4));
 console.log(sumTo(3));
 
 //2
-function findShort(string){
-	let shorterWord=string.split(' ').reduce(function(shorter,currentWord){
-		if(shorter.length==currentWord.length){
-			return shorter
-		}
-		return shorter.length<currentWord.length ? shorter:currentWord;
-
-	},);
-	return shorterWord
+function findShort(string) {
+  let shorterWord = string.split(" ").reduce(function (shorter, currentWord) {
+    if (shorter.length == currentWord.length) {
+      return shorter;
+    }
+    return shorter.length < currentWord.length ? shorter : currentWord;
+  });
+  return shorterWord;
 }
 
-
-
 const sentance = "Lorem ipsum dolor sit amet";
-console.log(findShort(sentance)); 
-console.log(findShort("Hello world")); 
-console.log(findShort("Hi")); 
-console.log(findShort("She is David's sister")); 
+console.log(findShort(sentance));
+console.log(findShort("Hello world"));
+console.log(findShort("Hi"));
+console.log(findShort("She is David's sister"));
